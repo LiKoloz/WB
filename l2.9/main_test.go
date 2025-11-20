f729bc7dda3cb4b9b66e02145d0657a0521e719a
Ilya <Ptvkzr_YbrekbyfH@mail.ru>
2025-11-20 09:21:40 +0300

    l2.9-2

--- l2.9/main_test.go
+++ l2.9/main_test.go
@@ -23,3 +23,25 @@ func TestUnpackingString2(t *testing.T) {
 		t.Error("error")
 	}
 }
+
+func TestUnpackingString3(t *testing.T) {
+	a := "qwe\\4\\5"
+	e := "qwe45"
+
+	r, err := unpacking_string(a)
+
+	if err != nil && r != e {
+		t.Error("error")
+	}
+}
+
+func TestUnpackingString4(t *testing.T) {
+	a := "qwe\\45"
+	e := "qwe44444"
+
+	r, err := unpacking_string(a)
+
+	if err != nil && r != e {
+		t.Error("error")
+	}
+}
