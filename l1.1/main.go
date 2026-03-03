6914c58f49bac346087fc93416a5b761bd20cc03
Ilya <Ptvkzr_YbrekbyfH@mail.ru>
2026-03-03 16:08:32 +0300

    l1.1 comments

--- l1.1/main.go
+++ l1.1/main.go
@@ -2,18 +2,23 @@ package main
 
 import "fmt"
 
+// Human класс
 type Human struct {
 	name string
 	age  int
 }
 
+//SayHello - вывод на консоль
 func (h Human) SayHello() {
 	fmt.Println("Hello ", h.name)
 }
+
+// GetHello - получение для теста
 func (h Human) GetHello() string {
 	return "Hello " + h.name
 }
 
+// Action - наследник
 type Action struct {
 	Human
 }
