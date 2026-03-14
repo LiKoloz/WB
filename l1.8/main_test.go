f7e7c75c6e7a72bfa191875c72c7112ee345523a
Ilya <Ptvkzr_YbrekbyfH@mail.ru>
2026-03-14 15:04:20 +0300

    pravki

--- l1.8/main_test.go
+++ l1.8/main_test.go
@@ -3,16 +3,16 @@ package main
 import "testing"
 
 func TestSetBit(t *testing.T) {
-	got := SetBit(5, 1) // устанавливаем 1-й бит в 1: 0101 -> 0111 = 7
+	got := SetBit(5, 1)
 	var exp int64 = 7
 
 	if got != exp {
-		t.Errorf("SetBit(5, 1) = %d; want %d", got, exp)
+		t.Errorf("Error")
 	}
 }
 
 func TestClearBit(t *testing.T) {
-	got := ClearBit(5, 0) // сбрасываем 1-й бит в 0: 0101 -> 0100 = 4
+	got := ClearBit(5, 0)
 	var exp int64 = 4
 
 	if got != exp {
