f5cf0ede989c8b79870d9073919b0d37a6384d3a
Ilya <Ptvkzr_YbrekbyfH@mail.ru>
2026-03-21 13:46:41 +0300

    pravki

--- l2.18/main.go
+++ l2.18/main.go
@@ -2,15 +2,29 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"strconv"
 
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/v5/middleware"
 	"l2.18/controllers"
 )
 
 func main() {
 	e := echo.New()
-
+	e.Use(middleware.RequestLoggerWithConfig(middleware.RequestLoggerConfig{
+		LogStatus: true,
+		LogURI:    true,
+		Skipper:   skipper,
+		BeforeNextFunc: func(c *echo.Context) {
+			c.Set("customValueFromContext", 42)
+		},
+		LogValuesFunc: func(c *echo.Context, v middleware.RequestLoggerValues) error {
+			value, _ := c.Get("customValueFromContext").(int)
+			fmt.Printf("REQUEST: uri: %v, status: %v, custom-value: %v\n", v.URI, v.Status, value)
+			return nil
+		},
+	}))
 	controllers.EventController(e)
 
 	port := flag.Int("port", 8080, "app port")
